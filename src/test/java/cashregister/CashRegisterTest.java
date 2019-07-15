edbfa086cabc3c503bad2c02d7b485f1c5e2133a
BbillyMai <1363543881@qq.com>
2019-07-15 22:49:03 +0800

    finished should print the real purchase when call process

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,32 +1,53 @@
 package cashregister;
 
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 public class CashRegisterTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    private void restoreStreams() {
+        System.setOut((originalOut));
+    }
 
     @Test
     public void should_print_the_real_purchase_when_call_process() {
-        //given
-        //when
-        //then
+
+        CashRegister cashRegister = new CashRegister(new Printer());
+        Purchase purchase = new Purchase(new Item[]{new Item("Apple", 1.0)});
+
+        cashRegister.process(purchase);
+
+        assertEquals("Apple\t1.0\n", outContent.toString());
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
-        //then
+
+        
     }
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
-        //given
-        //when
-        //then
+
+
     }
 
 }
